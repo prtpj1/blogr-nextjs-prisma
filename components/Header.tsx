a2083e8627eb7f6563b2b037f634f976440f837e
prtpj1 <prtpj1@gmail.com>
2024-01-04 12:03:56 -0300

    :yum: feat: add GitHub login :arrow_right: create schemas with Prisma :arrow_right: update views to load data from database :arrow_right: update schemas with tables required by NextAuth :arrow_right: add login functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { signOut, useSession } from 'next-auth/react';
 
 const Header: React.FC = () => {
   const router = useRouter();
   const isActive: (pathname: string) => boolean = (pathname) =>
     router.pathname === pathname;
+
+  const { data: session, status } = useSession();
 
   let left = (
     <div className="left">
@@ -20,9 +23,9 @@ const Header: React.FC = () => {
         }
 
         a {
-          text-decoration: none;
           color: #000;
           display: inline-block;
+          text-decoration: none;
         }
 
         .left a[data-active="true"] {
@@ -38,15 +41,147 @@ const Header: React.FC = () => {
 
   let right = null;
 
+  if (status === 'loading') {
+    left = (
+      <div className='left'>
+        <Link href="/">
+          <a className='bold' data-active={isActive('/')}>Feed</a>
+        </Link>
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+          .a {
+            color: var(--geist-foreground);
+            display: inline-block;
+            text-decoration: none;
+          }
+          .left a[data-active='true'] {
+            color: gray
+          }
+          a + a {
+            margin-left: 1rem
+          }
+        `}</style>
+      </div>
+    );
+    right = (
+      <div className='right'>
+        <p>Validating session...</p>
+        <style jsx>{`
+          .right {
+            margin-left: auto;
+          }
+        `}</style>
+      </div>
+    );
+  }
+
+  if (!session) {
+    right = (
+      <div className='right'>
+        <Link href='/api/auth/signin'>
+          <a data-active={isActive('/signup')}>Log in</a>
+        </Link>
+        <style jsx>{`
+          a {
+            color: var(--geist-foreground);
+            display: inline-block;
+            text-decoration: none;
+          }
+          a + a {
+            margin-left: 1rem;
+          }
+          .right {
+            margin-left: auto;
+          }
+          .right a {
+            border: 1px solid var(--geist-foreground);
+            border-radius: 3px;
+            padding: 0.5rem 1rem;
+          }
+        `}</style>
+      </div>
+    )
+  }
+
+  if (session) {
+    left = (
+      <div className='left'>
+        <Link href='/'>
+          <a className='bold' data-active={isActive('/')}>Feed</a>
+        </Link>
+        <Link href='/drafts'>
+          <a data-active={isActive('/drafts')}>My Drafts</a>
+        </Link>
+        <style jsx>{`
+          .bold {
+            font-weight: bold;
+          }
+          a {
+            color: var(--geist-foreground);
+            display: inline-block;
+            text-decoration: none;
+          }
+          .left a[data-active='true'] {
+            color: gray;
+          }
+          a + a {
+            margin-left: 1rem;
+          }
+        `}</style>
+      </div>
+    );
+    right = (
+      <div className='right'>
+        <p>{session.user.name} ({session.user.email})</p>
+        <Link href='/create'>
+          <button>
+            <a> New Post</a>
+          </button>
+        </Link>
+        <button onClick={() => signOut()}>
+          <a>Log Out</a>
+        </button>
+        <style jsx>{`
+          a {
+            color: var(--geist-foreground);
+            display: inline-block;
+            text-decoration: none;
+          }
+          p {
+            display: inline-block;
+            font-size: 13px;
+            padding-right: 1rem;
+          }
+          a + a {
+            margin-left: 1rem;
+          }
+          .right {
+            margin-left: auto;
+          }
+          .right a {
+            border: 1px solid var(--geist-foreground);
+            border-radius: 3px;
+            padding: 0.5rem 1rem;
+          }
+          button {
+            border: none;
+          }
+        `}</style>
+      </div>
+    )
+  }
+
   return (
     <nav>
       {left}
       {right}
       <style jsx>{`
         nav {
+          align-items: center;
           display: flex;
           padding: 2rem;
-          align-items: center;
         }
       `}</style>
     </nav>
